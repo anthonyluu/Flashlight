fdf73e338107125d1a01dd16d75ddcf251d75ba3
Anthony <anthony_luu99@hotmail.com>
2014-05-26 23:54:48 -0400

    mostly finished, forgot to push

--- src/com/example/flashlight/MainActivity.java
+++ src/com/example/flashlight/MainActivity.java
@@ -2,22 +2,25 @@ package com.example.flashlight;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 	
 	ImageButton buttonSwitch;
 	
-	private Camera camera;
-	private boolean isFlashlightOn;
+	private Camera c;
+	private boolean isFlashlightOn = false;
 	private boolean hasFlashlight;
-	Parameters params;
+	Parameters p;
 	MediaPlayer mp;
 
 	@Override
@@ -45,16 +48,60 @@ public class MainActivity extends Activity {
 			AlertDialog alert = alertBuilder.create();
 			alert.show();
 		}
+		getCamera();
+		
+		buttonSwitch.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				toggleSwitch();				
+			}
+		});
 		
 	}
 	
-	// not working yet
+	
 	private void toggleSwitch() {
+		Log.d("buttonClick", "clicked");
+		
 		if(isFlashlightOn) {
-			buttonSwitch.setImageResource(R.drawable.button_on);
-		}
-		else {
+			turnOff();
 			buttonSwitch.setImageResource(R.drawable.button_off);
 		}
+		else {
+			turnOn();
+			buttonSwitch.setImageResource(R.drawable.button_on);
+			
+		}
+		
+		
+	}
+	
+	private void getCamera() {
+		if(c == null) {
+			try {
+				c = Camera.open();
+				p = c.getParameters();
+			}
+			catch(RuntimeException e) {
+				Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
+			}
+		}
+	}
+	
+	private void turnOn() {
+		p = c.getParameters();
+		p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		c.setParameters(p);
+		c.startPreview();
+		isFlashlightOn = true;
+	}
+	
+	private void turnOff() {
+		p = c.getParameters();
+		p.setFlashMode(Parameters.FLASH_MODE_OFF);
+		c.setParameters(p);
+		c.stopPreview();
+		isFlashlightOn = false;
 	}
 }
